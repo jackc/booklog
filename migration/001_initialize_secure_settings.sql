d7c789588371d7be5356feacb34e3270effe1010
Jack Christensen <jack@jackchristensen.com>
2019-07-13 16:55:54 -0500

    Tighten database permissions

--- migration/001_initialize_secure_settings.sql
+++ migration/001_initialize_secure_settings.sql
@@ -1,1 +1,14 @@
-select 'TODO';
+-- Note that "public" has two meanings. It is the name of a schema but it is all a keywork meaning all roles.
+
+do $$
+declare
+  dbname text;
+begin
+  select current_database() into dbname;
+  execute 'revoke connect on database ' || quote_ident(dbname) || ' from public';
+  execute 'grant connect on database ' || quote_ident(dbname) || ' to {{.app_user}}';
+end
+$$;
+
+revoke all privileges on schema public from public;
+grant usage on schema public to {{.app_user}};
