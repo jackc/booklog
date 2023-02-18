40aca02825913ecdd040144bdf0e4e914a092d03
Jack Christensen <jack@jackchristensen.com>
2023-02-18 08:25:58 -0600

    Use pgxrecord.Select

--- data/analysis.go
+++ data/analysis.go
@@ -5,6 +5,7 @@ import (
 	"time"
 
 	"github.com/jackc/pgx/v5"
+	"github.com/jackc/pgxrecord"
 )
 
 type BooksPerTimeItem struct {
@@ -13,15 +14,25 @@ type BooksPerTimeItem struct {
 }
 
 func BooksPerYear(ctx context.Context, db dbconn, userID int64) ([]BooksPerTimeItem, error) {
-	rows, _ := db.Query(ctx, "select date_trunc('year', finish_date), count(*) from books where user_id=$1 group by 1 order by 1 desc", userID)
-	return pgx.CollectRows(rows, pgx.RowToStructByPos[BooksPerTimeItem])
+	return pgxrecord.Select(
+		ctx,
+		db,
+		"select date_trunc('year', finish_date), count(*) from books where user_id=$1 group by 1 order by 1 desc",
+		[]any{userID},
+		pgx.RowToStructByPos[BooksPerTimeItem],
+	)
 }
 
 func BooksPerMonthForLastYear(ctx context.Context, db dbconn, userID int64) ([]BooksPerTimeItem, error) {
-	rows, _ := db.Query(ctx, `select months, count(books.id)
+	return pgxrecord.Select(
+		ctx,
+		db,
+		`select months, count(books.id)
 from generate_series(date_trunc('month', now() - '1 year'::interval), date_trunc('month', now()), '1 month') as months
 	left join books on date_trunc('month', finish_date) = months and user_id=$1
 group by 1
-order by 1 desc`, userID)
-	return pgx.CollectRows(rows, pgx.RowToStructByPos[BooksPerTimeItem])
+order by 1 desc`,
+		[]any{userID},
+		pgx.RowToStructByPos[BooksPerTimeItem],
+	)
 }
